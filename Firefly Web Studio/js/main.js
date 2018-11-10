2e5845787097953f5241b4050c18ce5d6003902b
UkiWoDao <41085559+UkiWoDao@users.noreply.github.com>
2018-11-10 19:56:38 +0100

    Add files via upload

---
+++ Firefly Web Studio/js/main.js
@@ -0,0 +1,9 @@
+function openSM(){
+  document.getElementById('mysidemenu').style.width = '300px';
+  document.getElementById('pg-content').style.marginLeft = '300px';
+}
+
+function closeSM(){
+  document.getElementById('mysidemenu').style.width = '0';
+  document.getElementById('pg-content').style.marginLeft = '0';
+}
