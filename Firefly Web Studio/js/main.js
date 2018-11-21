fbaf795b9f2f5203529a812c045b6dd1e1a26d9e
UkiWoDao <41085559+UkiWoDao@users.noreply.github.com>
2018-11-21 14:27:20 +0100

    logo animation added

--- Firefly Web Studio/js/main.js
+++ Firefly Web Studio/js/main.js
@@ -4,15 +4,18 @@ let toggleNav = function() {
   let getSidebar = document.querySelector('.sidemenu');
   let getContent = document.querySelector('.pg-content');
   let getIcon = document.querySelector('.menu-container');
+  let getLogo = document.querySelector('.logo');
 
   if (toggleNavStatus === false) {
     getSidebar.style.width = '300px';
     getIcon.style.marginLeft = '220px';
+    getLogo.style.paddingLeft = '60px';
     toggleNavStatus = true;
 
   } else if (toggleNavStatus === true) {
     getSidebar.style.width = '0px';
     getIcon.style.marginLeft = '0px';
+    getLogo.style.paddingLeft = '0px';
     toggleNavStatus = false;
     }
   }
