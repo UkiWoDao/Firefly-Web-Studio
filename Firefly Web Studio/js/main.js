affbfe8e299795ea21bf8338c3693c7fb1b444b9
UkiWoDao <41085559+UkiWoDao@users.noreply.github.com>
2018-12-12 16:02:35 +0100

    added js list item animation

--- Firefly Web Studio/js/main.js
+++ Firefly Web Studio/js/main.js
@@ -5,21 +5,25 @@ let toggleNav = function() {
   let getContent = document.querySelector('.pg-content');
   let getIcon = document.querySelector('.menu-container');
   let getLogo = document.querySelector('.logo');
+  let getListItems = document.getElementsByClassName('list-items');
 
   if (toggleNavStatus === false) {
     getSidebar.style.width = '300px';
     getIcon.style.marginLeft = '220px';
     getLogo.style.paddingLeft = '60px';
     toggleNavStatus = true;
+    $(getListItems).addClass('animate');
+  }
 
-  } else if (toggleNavStatus === true) {
+  else if (toggleNavStatus === true) {
     getSidebar.style.width = '0px';
     getIcon.style.marginLeft = '0px';
     getLogo.style.paddingLeft = '0px';
     toggleNavStatus = false;
-    }
+    $(getListItems).removeClass('animate');
   }
+}
 
 function myFunction(x) {
-      x.classList.toggle("change");
+    x.classList.toggle("change");
   }
